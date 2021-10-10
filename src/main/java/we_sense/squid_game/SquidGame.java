67e9c5883a25d5a9cfce3f0b56ad3708ee7e09d7
ISenseHostility <yaboijarno@gmail.com>
2021-10-11 00:39:45 +0200

    test version

--- src/main/java/we_sense/squid_game/SquidGame.java
+++ src/main/java/we_sense/squid_game/SquidGame.java
@@ -1,17 +1,48 @@
 package we_sense.squid_game;
 
+import org.bukkit.Server;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 public final class SquidGame extends JavaPlugin {
+    private Server server;
+    private boolean mayMove = true;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        this.server = this.getServer();
+        runnable.runTaskTimer(this, secondsToTicks(5), secondsToTicks(randomIntBetween(15, 30)));
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
+    }
+
+    BukkitRunnable runnable = new BukkitRunnable() {
+        @Override
+        public void run() {
+            if (server.getOnlinePlayers().size() != 0) {
+                Player player = (Player) server.getOnlinePlayers().toArray()[0];
+                if (mayMove) {
+                    mayMove = false;
+                    player.sendMessage("false");
+                } else {
+                    mayMove = true;
+                    player.sendMessage("true");
+                }
+            }
+        }
+    };
+
+    public static int randomIntBetween(int min, int max) {
+        return ThreadLocalRandom.current().nextInt(min, max);
+    }
+
+    public static int secondsToTicks(int seconds) {
+        return seconds * 20;
     }
 }
