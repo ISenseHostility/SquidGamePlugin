1f386fd5fa1d0014f3df25cfa94be5f78266ab4a
ISenseHostility <yaboijarno@gmail.com>
2021-10-12 18:42:36 +0200

    added custom death message when dying from a game

--- src/main/java/we_sense/squid_game/events/SquidGameListener.java
+++ src/main/java/we_sense/squid_game/events/SquidGameListener.java
@@ -3,10 +3,13 @@ package we_sense.squid_game.events;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import we_sense.squid_game.rlgl.RedLightGreenLight;
+import we_sense.squid_game.utils.SquidGameUtil;
 
 public class SquidGameListener implements Listener {
+    private final SquidGameUtil squidGameUtil = new SquidGameUtil();
 
     @EventHandler
     public void onPlayerMove(final PlayerMoveEvent event) {
@@ -18,10 +21,22 @@ public class SquidGameListener implements Listener {
 
                 if (!mayMove) {
                     if (redLightGreenLight.getActivePlayers().contains(player)) {
+                        squidGameUtil.setDeathByPlugin(player, 1);
                         player.setHealth(0);
                         redLightGreenLight.getActivePlayers().remove(player);
+                        squidGameUtil.setDeathByPlugin(player, 0);
                     }
                 }
+            }
+        }
+    }
+
+    @EventHandler
+    public void onPlayerDeath(final PlayerDeathEvent event) {
+        Player player = event.getEntity().getPlayer();
+        if (player != null) {
+            if (squidGameUtil.getDeathByPlugin(player) == 1) {
+                event.setDeathMessage(event.getEntity().getDisplayName() + " was shot in the " + squidGameUtil.getRandomBodyPart() + ".");
             }
         }
     }
--- src/main/java/we_sense/squid_game/utils/SquidGameUtil.java
+++ src/main/java/we_sense/squid_game/utils/SquidGameUtil.java
@@ -1,12 +1,17 @@
 package we_sense.squid_game.utils;
 
+import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
+import org.bukkit.persistence.PersistentDataType;
+import we_sense.squid_game.SquidGame;
 
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class SquidGameUtil {
     private final Random random = new Random();
+    private final SquidGame squidGame = SquidGame.getInstance();
+    private String[] bodyParts = {"head", "leg", "arm", "neck", "foot", "hand", "chest"};
 
     public int secondsToTicks(int seconds) {
         return seconds * 20;
@@ -17,7 +22,33 @@ public class SquidGameUtil {
     }
 
     public int randomIntBetween(int min, int max) {
-        return random.nextInt(max-min)+min;
+        return random.nextInt(max - min) + min;
+    }
 
+    public NamespacedKey getKey(String name) {
+        return new NamespacedKey(squidGame ,name);
+    }
+
+    public void setDeathByPlugin(Player player, int on) {
+        player.getPersistentDataContainer().set(
+                    getKey("wasKilledByPlugin"),
+                    PersistentDataType.INTEGER,
+                    on
+                );
+    }
+
+    public int getDeathByPlugin(Player player) {
+        if (player.getPersistentDataContainer().get(getKey("wasKilledByPlugin"), PersistentDataType.INTEGER) != null) {
+            return player.getPersistentDataContainer().get(
+                    getKey("wasKilledByPlugin"),
+                    PersistentDataType.INTEGER
+            );
+        } else {
+            return 0;
+        }
+    }
+
+    public String getRandomBodyPart() {
+        return bodyParts[randomIntBetween(0, 6)];
     }
 }
